818763b911d510d5bf5ef050c3c057a0ba694142
Melissa Lawrence <melissaglawrence8@gmail.com>
2022-01-11 22:24:30 -0800

    refactor: api headers

--- my-app/src/utils/WeatherApi.js
+++ my-app/src/utils/WeatherApi.js
@@ -1,6 +1,7 @@
 class Api {
-  constructor(apiKey) {
+  constructor(apiKey, headers) {
     this._apiKey = apiKey;
+    this._headers = headers;
   }
 
   _handleResponse = (res) => {
@@ -15,10 +16,7 @@ class Api {
     return fetch(
       `/data/2.5/weather?q=${cityName}&units=imperial&appid=${this._apiKey}`,
       {
-        headers: {
-          Authorization: `${this._apiKey}`,
-          'Content-Type': 'application/json',
-        },
+        headers: this._headers,
       },
     ).then(this._handleResponse);
   }
@@ -27,10 +25,7 @@ class Api {
     return fetch(
       `/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${this._apiKey}`,
       {
-        headers: {
-          Authorization: `${this._apiKey}`,
-          'Content-Type': 'application/json',
-        },
+        headers: this._headers,
       },
     ).then(this._handleResponse);
   }
@@ -40,15 +35,15 @@ class Api {
       `/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${this._apiKey}
 `,
       {
-        headers: {
-          Authorization: `${this._apiKey}`,
-          'Content-Type': 'application/ld+json',
-        },
+        headers: this._headers,
       },
     ).then(this._handleResponse);
   }
 }
 
-const api = new Api('f9dca6765762554e8625629f29a25f91');
+const api = new Api('f9dca6765762554e8625629f29a25f91', {
+  Authorization: 'f9dca6765762554e8625629f29a25f91',
+  'Content-Type': 'application/json',
+});
 
 export default api;
